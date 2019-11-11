df97962fdf45b81584f9bcf770872840bafe0451
Jacob Cassady <jtcass01@louisville.edu>
2019-11-10 20:50:28 -0500

    Finished with Random11 tests for now.  Moving on to Random22 with some refined parameters

--- Project 6/src/runner.py
+++ Project 6/src/runner.py
@@ -3,17 +3,17 @@ import datetime
 import itertools
 
 def run_full_tests():
-	datasets = list(["Random11", "Random22", "Random44", "Random77", "Random97", "Random222"])
-	dataset_sizes = list([11, 22, 44, 77, 97, 222])
+	datasets = list(["Random22", "Random44", "Random77", "Random97", "Random222"])
+	dataset_sizes = list([22, 44, 77, 97, 222])
 	assert len(datasets) == len(dataset_sizes)
 
 	for dataset, dataset_size in zip(datasets, dataset_sizes):
-		population_sizes_per_genetic_algorithm = [5, 10, 25, 50, 75]
-		epoch_thresholds = [10, 25, 50, 75, 100]
+		population_sizes_per_genetic_algorithm = [10, 25, 50, 75]
+		epoch_thresholds = [25, 50, 100]
 		crossover_probabilities = [0.2, 0.4, 0.6, 0.8]
 		mutation_probabilities = [0.01, 0.1, 0.25, 0.5]
-		number_of_depots_possibilities = list(range(1, dataset_size, int(dataset_size/10)))
-		number_of_customers_possibilities = list(range(int(dataset_size/2), dataset_size-1, int(dataset_size/10)))
+		number_of_depots_possibilities = list(range(1, dataset_size, int(dataset_size/10)+1))
+		number_of_customers_possibilities = list(range(int(dataset_size/2), dataset_size-1, int(dataset_size/10)+1))
 		pop_epoch_crossover_mutation = list(itertools.product(population_sizes_per_genetic_algorithm, epoch_thresholds, crossover_probabilities, mutation_probabilities, number_of_depots_possibilities, number_of_customers_possibilities))
 
 		for population_size_per_genetic_algorithm, epoch_threshold, crossover_probability, mutation_probability, number_of_depots, number_of_customers in pop_epoch_crossover_mutation:
